8609773a17b9afb5427f7057ee9f042d0b8b5c49
Nimesh kewat <nimeshgamer37@gmail.com>
2026-09-10 22:10:45 +0530

    add home route

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,10 @@ app.get("/", (req: Request, res: Response) => {
   res.json({ success: true, message: "Api Working !" });
 });
 
+app.get("/home", (req: Request, res: Response) => {
+  res.json({ success: true, message: "Home page" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
