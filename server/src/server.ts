e94695db2fac55314c30561330deaf36452ba1b6
Nimesh kewat <nimeshgamer37@gmail.com>
2026-09-10 22:09:36 +0530

    Initial commit

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+import express, { type Request, type Response } from "express";
+
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ success: true, message: "Api Working !" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
